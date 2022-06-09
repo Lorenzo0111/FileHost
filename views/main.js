7eee117e7ab9123e1a3daea09ff7f4898ad33ce2
Lorenzo <69311874+Lorenzo0111@users.noreply.github.com>
2022-06-09 18:49:45 +0200

    Changed graphic

--- views/main.js
+++ views/main.js
@@ -9,7 +9,7 @@ function loadData() {
     }).then(response => {
         const pages = response.data;
         const navbarItems = pages.map(page => {
-            return `<li href="#${page}" style="margin: 10px; text-align: center; list-style: none; min-height: 32px; border-radius: 3px; border: black solid 1px;">${page}</li>`;
+            return `<li href="#${page}" style="margin: 10px; text-align: center; list-style: none; min-height: 32px; border-radius: 3px; border: white solid 1px; color:white;">${page}</li>`;
         });
         navbar.append(navbarItems);
     
